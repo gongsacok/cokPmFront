c8499786ee2d069b0e35943eb3134706be964c82
jeonga96 <wazzang96jeonga@gmail.com>
2023-06-12 13:32:43 +0900

    [Modify] grandopen 문구 수정

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -13,15 +13,6 @@ const FaqBox = lazy(() => import("../component/FaqBox.js"));
 const CertificationBox = lazy(() => import("../component/CertificationBox.js"));
 const Footer = lazy(() => import("../component/Footer.js"));
 
-// import VisualBox from "../component/VisualBox";
-// import UserMentionBox from "../component/UserMentionBox";
-// import AppScreenSliderBox from "../component/AppScreenSliderBox";
-// import GpsBox from "../component/GpsBox";
-// import BusinessLinkBox from "../component/BusinessLinkBox";
-// import FaqBox from "../component/FaqBox";
-// import CertificationBox from "../component/CertificationBox";
-// import Footer from "../component/Footer";
-
 export default function Promotion() {
   return (
     <div>
